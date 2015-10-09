e3c5dd4e01b670dc2f9bb14f85e2e07ff5337566
Shane Harvey <shane6harvey@yahoo.com>
2015-10-09 16:59:47 -0400

    Add some example web request blocking/modification to background.

--- app/scripts/background.js
+++ app/scripts/background.js
@@ -7,3 +7,37 @@ chrome.runtime.onInstalled.addListener(function (details) {
 chrome.browserAction.setBadgeText({text: '\'Allo'});
 
 console.log('\'Allo \'Allo! Event Page for Browser Action');
+
+
+// https://developer.chrome.com/extensions/webRequest#event-onBeforeSendHeaders
+var requests = [];
+
+chrome.webRequest.onBeforeSendHeaders.addListener(
+  function(details) {
+    for (var i = details.requestHeaders.length - 1; i >= 0; --i) {
+      // Example code to delete user-agent and refer from requests
+      // if (details.requestHeaders[i].name === 'User-Agent') {
+      //   details.requestHeaders.splice(i, 1);
+      // } else if (details.requestHeaders[i].name === 'Referer') {
+      //   details.requestHeaders.splice(i, 1);
+      // }
+    }
+    console.log(details);
+    return {requestHeaders: details.requestHeaders};
+  },
+  {urls: ['<all_urls>']},
+  ['blocking', 'requestHeaders']);
+
+chrome.webRequest.onBeforeRequest.addListener(
+  function(details) {
+    console.log(details);
+    requests.push(details);
+    var str = JSON.stringify(details);
+    if(str.indexOf('shane') !== -1) {
+      console.log('Blocking request to ' + details.url);
+      return {cancel: true};
+    }
+    return {cancel: false};
+  },
+  {urls: ['<all_urls>']},
+  ['blocking', 'requestBody']);
