9e02c743ce0cc872814f92bb776dff363801b8c0
Michael Liuzzi <michaelliuzzi@MacBook-Pro.local>
2015-10-16 18:01:55 -0400

    First backend commit

---
+++ backend/database.py
@@ -0,0 +1,34 @@
+"""
+The database schema for our backend:
+
+  Name   | Option Tally | Option Tally |....etc|
+-----------------------------------------------
+         |              |              |       |
+-----------------------------------------------
+
+"""
+
+class stopleak_db:
+    def __init__(self, db_name):
+        conn = sqlite3.connect(db_name)
+        c = conn.cursor()
+
+    def record_tally(self, domain, choice):
+        #avoid sql injection with param substitution
+        self.c.execute('UPDATE domain_data SET ? = ? + 1 where domain = "?"', (choice, choice, domain))
+        self.conn.commit()
+
+    def record_add_domain(self, domain):
+        self.c.execute('INSERT INTO domain_data (domain) VALUES + (?)', (domain,))
+        self.conn.commit()
+        
+    def record_get_row(self, domain):
+        self.c.execute('SELECT * FROM domain_data WHERE domain = ?', (domain,))
+        row = self.c.fetchone()
+        
+        
+                
+
+
+
+
---
+++ backend/server.py
@@ -0,0 +1,30 @@
+import asyncio
+import websockets
+import database
+import sqlite3
+
+DB_NAME = 'test.db'
+
+def handle_request(websocket, path):
+    request = yield from websocket.recv()
+
+    print(request)
+    
+    if request == "record_tally":
+        backend.record_tally()
+        
+    if request == "record_add_domain":
+        backend.record_add_domain()
+    if request == "record_get_best_option":
+        backend.record_get_best_option()
+    
+
+#server = websockets.serve(handle_request, 'localhost', 8765)
+server = websockets.serve(handle_request, '0.0.0.0', 8765)
+#backend = stopleak_db(DB_NAME)
+
+asyncio.get_event_loop().run_until_complete(server)
+asyncio.get_event_loop().run_forever()
+
+    
+
