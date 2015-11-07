f760f1727d8d25941ed7434825405e55228fcede
Scott Harvey <scott.harvey@stonybrook.edu>
2015-11-07 13:51:30 -0500

    Add arguments to the server.
    Uses argparse to add a few options. The help message includes the
    defaults.

--- backend/README.md
+++ backend/README.md
@@ -1,4 +1,13 @@
+# Running the server
+* For a list of all options:
+./server.py -h
+
+## Example
+./server.py -s localhost -p 8765 -f log.log -l debug -d tooling/test.db
+
+
 # Dependencies
 
+* python3
 * To install all dependencies run the following command
 	* `pip3 install -r requirements.txt`
--- backend/server.py
+++ backend/server.py
@@ -7,9 +7,15 @@ import asyncio
 import websockets
 import json
 import logging
+import argparse
+import sys
 import database
 
-DB_NAME = 'test.db'
+DEFAULT_DB_NAME = 'test.db'
+DEFAULT_LOG_NAME = 'stopLeakServer.log'
+DEFAULT_LOG_LVL = 'info'
+DEFAULT_HOST = 'localhost'
+DEFAULT_PORT = 8765
 
 
 class StopLeak(object):
@@ -24,11 +30,39 @@ class StopLeak(object):
 def main():
     global stopLeak
 
-    init_logging()
+    argparser = argparse.ArgumentParser(description='StopLeak Server.')
+    argparser.add_argument('-d', '--db', action='store', dest='db_name',
+                           default=DEFAULT_DB_NAME,
+                           help='path to the sqlite3 database '
+                                '(default: {})'.format(DEFAULT_DB_NAME))
+    argparser.add_argument('-f', '--log-file', action='store', dest='log_file',
+                           default=DEFAULT_LOG_NAME,
+                           help='path to the log file (truncates current content)'
+                                '(default: {})'.format(DEFAULT_LOG_NAME))
+    argparser.add_argument('-l', '--log-lvl', action='store',  dest='log_level',
+                           default=DEFAULT_LOG_LVL,
+                           help='logging level, in decreasing verbosity: '
+                                'debug, info, warning, error, critical '
+                                '(default: {})'.format(DEFAULT_LOG_LVL))
+    argparser.add_argument('-s', '--host', action='store', dest='host',
+                           default=DEFAULT_HOST,
+                           help='server hostname '
+                                '(default: {})'.format(DEFAULT_HOST))
+    argparser.add_argument('-p', '--port', action='store', dest='port',
+                           default=DEFAULT_PORT,
+                           help='server port '
+                                '(default: {})'.format(DEFAULT_PORT))
+    argparser.add_argument('-v', '--version', action='version', version='StopLeak Server v0.1')
 
-    stopLeak = StopLeak(DB_NAME, 'localhost', 8765)
+    args = argparser.parse_args()
+
+    level = log_level_to_int(args.log_level)
+    init_logging(args.log_file, level)
+
+    stopLeak = StopLeak(args.db_name, args.host, args.port)
+    logging.info('Listening at: %s:%s', args.host, args.port)
     try:
-        # Start the backend
+        # Start the asyncio event loop which runs the websockets request handler
         asyncio.get_event_loop().run_until_complete(stopLeak.server)
         asyncio.get_event_loop().run_forever()
     except KeyboardInterrupt:
@@ -37,16 +71,34 @@ def main():
             stopLeak.db.close()
 
 
-def init_logging():
+def log_level_to_int(str_log_level):
+    """
+    Maps strings to the log-level constants of the logging module.
+    'info' -> logging.INFO
+    'debug' -> logging.DEBUG
+    ...
+    """
+    numeric_level = getattr(logging, str_log_level.upper(), None)
+    if not isinstance(numeric_level, int):
+        print('Invalid log level: {}'.format(str_log_level), file=sys.stderr)
+        exit(1)
+    else:
+        return numeric_level
+
+
+def init_logging(log_file, log_level):
+    """
+    Initialize the logging module.
+    """
     fmt = '%(asctime)s - %(name)s - %(levelname)s: %(message)s'
-    logging.basicConfig(filename='server.log', format=fmt, level=logging.DEBUG)
+    logging.basicConfig(filename=log_file, filemode='w', format=fmt, level=log_level)
     init_ws_logging()
 
 
 def init_ws_logging():
     """
     The websockets module logs errors to 'websockets.server' so let's print those.
-    """""
+    """
     logger = logging.getLogger('websockets.server')
     logger.setLevel(logging.ERROR)
     logger.addHandler(logging.StreamHandler())
