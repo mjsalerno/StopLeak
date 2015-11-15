2d25c242f06a4ec446920c8e9fd319473a02a49d
Michael Salerno <michael.j.salerno@stonybrook.edu>
2015-11-15 17:12:25 -0500

    added storage js

---
+++ app/scripts/storage.js
@@ -0,0 +1,26 @@
+/**
+ * Created by michael on 11/15/15.
+ */
+'use strict';
+
+var stopleak = stopleak || {};
+
+stopleak.blocks = stopleak.blocks || {};
+stopleak.tabDomain = stopleak.tabDomain || {};
+stopleak.PIIData = stopleak.PIIData || [];
+stopleak.deny = stopleak.deny || [];
+stopleak.allow = stopleak.allow || [];
+stopleak.scrub = stopleak.scrub || [];
+stopleak.tabQueue = stopleak.tabQueue || [];
+
+/**
+ * Load user data from storage.
+ */
+function getUserData() {
+    chrome.storage.sync.get(null, function (list) {
+        stopleak.PIIData = list[BLOCKED_STRINGS];
+        stopleak.deny = list[DENY];
+        stopleak.allow = list[ALLOW];
+        stopleak.scrub = list[SCRUB];
+    });
+}
