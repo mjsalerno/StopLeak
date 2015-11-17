ec2f4e8c3e2c4f6e05d75fb80dddda83099b76d9
Shane Harvey <shane6harvey@yahoo.com>
2015-11-17 03:01:39 -0500

    Rearranged network code.
    Each request is buffered until it gets to onBeforeSendHeaders.
    After buffering the request we check the user preferences and inspect
    the request for PII data. Then we will know whether to block, scrub,
    or allow.

--- app/scripts/utils.js
+++ app/scripts/utils.js
@@ -11,3 +11,26 @@ stopleak.getDomain = function (url) {
 
     return host.split('.').slice(-2).join('.');
 };
+
+/**
+ * Return the value of the headerName in the provided request.
+ * @param {array} requestHeaders Array of HTTPHeader objects
+ * @param {string} headerName
+ * @returns {any} Returns the value or binaryValue of the header in the
+ *     request, or null if he request is not present
+ */
+stopleak.requestGetHeader = function (requestHeaders, headerName) {
+    if (requestHeaders instanceof Array) {
+        for (var j = 0; j < requestHeaders.length; ++j) {
+            var header = requestHeaders[j];
+            if (header.name === headerName) {
+                if (header.hasOwnProperty('value')) {
+                    return header.value;
+                } else if (header.hasOwnProperty('binaryValue')) {
+                    return header.binaryValue;
+                }
+            }
+        }
+    }
+    return null;
+};
