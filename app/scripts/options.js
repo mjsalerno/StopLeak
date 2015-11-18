de4097441cf80eaec5d1881434b18456ef9f4cf4
Michael Salerno <michael.j.salerno@stonybrook.edu>
2015-11-17 19:00:42 -0500

    work in prograss options page

--- app/scripts/options.js
+++ app/scripts/options.js
@@ -18,7 +18,7 @@ function addStringToUI(str, idd, key) {
             if (index > -1) {
                 filters.splice(index, 1);
                 var a  = {};
-                a[key]=filters;
+                a[key] = filters;
                 chrome.storage.sync.set(a, function() {
                     row.parentNode.removeChild(row);
                 });
@@ -30,68 +30,159 @@ function addStringToUI(str, idd, key) {
     cell0.appendChild(btn);
 }
 
+function addStringToCS(src, dst, action) {
+    var table = document.getElementById('cs-tbl');
+    var row = table.insertRow(0);
+    var cell0 = row.insertCell(0);
+    var cell1 = row.insertCell(1);
+    var cell2 = row.insertCell(2);
+    cell0.innerHTML = src;
+    cell1.innerHTML = dst;
+    cell2.innerHTML = action;
+
+    /*var btn = document.createElement('button');
+    btn.innerHTML = 'Delete';
+    btn.type = 'button';
+    btn.onclick = function() { // Note this is a function
+        chrome.storage.sync.get(null, function(items) {
+            var filters = items[key];
+            var index = filters.indexOf(str);
+            if (index > -1) {
+                filters.splice(index, 1);
+                var a  = {};
+                a[key]=filters;
+                chrome.storage.sync.set(a, function() {
+                    row.parentNode.removeChild(row);
+                });
+            } else {
+                console.log('could not remove the filter: ' + str);
+            }
+        });
+    };
+    cell0.appendChild(btn);*/
+}
+
 function refreshSetting(idd, key) {
     chrome.storage.sync.get(null, function(items) {
-        if(!items[key]) {
+        if (!items[key]) {
             return;
         }
         var filters = items[key];
         document.getElementById(idd).innerHTML = '';
-        console.log(filters);
         for (var i = 0; i < filters.length; i++) {
             addStringToUI(filters[i], idd, key);
         }
     });
 }
 
+function refreshCustSettings() {
+    chrome.storage.sync.get(null, function(items) {
+        if (!item[CUSTOM_SETTINGS]) {
+            return;
+        }
+        var maps = items[CUSTOM_SETTINGS];
+        document.getElementById('cs-tbl').innerHTML = '';
+        for (var i = 0; i < maps.length; i++) {
+            //addStringToUI(filters[i], idd, key);
+            var map = maps[i];
+            addStringToCS(map.src, map.dst, map.action);
+        }
+    });
+}
+
 function addSetting(inId, tblId, key) {
     var newFilter = document.getElementById(inId).value;
-    if(!newFilter) {
+    if (!newFilter) {
         return;
     }
     chrome.storage.sync.get(null, function(items) {
         var filters = [];
         if (items[key]) {
             filters = items[key];
-            if(filters.indexOf(newFilter) > -1) {
+            if (filters.indexOf(newFilter) > -1) {
                 return;
             }
         }
         filters.push(newFilter);
         var a  = {};
-        a[key]=filters;
+        a[key] = filters;
         chrome.storage.sync.set(a, function() {
             refreshSetting(tblId, key);
         });
     });
 }
 
+function addCustSetting() {
+    var src = document.getElementById('new-cs-src').value;
+    var dst = document.getElementById('new-cs-dst').value;
+    var action = document.getElementById('select-action').value;
+    if (!src || !dst || !action) {
+        return;
+    }
+    chrome.storage.sync.get(null, function(items) {
+        var filters = [];
+        if (items[CUSTOM_SETTINGS]) {
+            custSett = items[CUSTOM_SETTINGS];
+            if (custSett.indexOf(custSett) > -1) {
+                return;
+            }
+        }
+        filters.push(newFilter);
+        var a  = {};
+        a[key] = filters;
+        chrome.storage.sync.set(a, function() {
+            refreshSetting(tblId, key);
+        });
+    });
+}
+
+function addActions() {
+    var x = document.getElementById('select-action');
+
+    var option = document.createElement('option');
+    option.text = ACTION_ALLOW;
+    x.add(option);
+
+    option = document.createElement('option');
+    option.text = ACTION_DENY;
+    x.add(option);
+
+    option = document.createElement('option');
+    option.text = ACTION_SCRUB;
+    x.add(option);
+}
+
 function clearFilters() {
-    chrome.storage.sync.clear();
+    chrome.storage.sync.remove(BLOCKED_STRINGS);
     document.getElementById('filter-tbl').innerHTML = '';
 }
 
 function clearAllows() {
-    chrome.storage.sync.clear();
+    chrome.storage.sync.remove(ALLOW);
     document.getElementById('allow-tbl').innerHTML = '';
 }
 
 function clearDeny() {
-    chrome.storage.sync.clear();
+    chrome.storage.sync.remove(DENY);
     document.getElementById('deny-tbl').innerHTML = '';
 }
 
 function clearScrub() {
-    chrome.storage.sync.clear();
+    chrome.storage.sync.remove(SCRUB);
     document.getElementById('scrub-tbl').innerHTML = '';
 }
 
 function clearSWWL() {
-    chrome.storage.sync.clear();
+    chrome.storage.sync.remove(SWWL);
     document.getElementById('swwl-tbl').innerHTML = '';
 }
 
-document.addEventListener('DOMContentLoaded', function () {
+function clearCustSettings() {
+    chrome.storage.sync.remove(SWWL);
+    document.getElementById('swwl-tbl').innerHTML = '';
+}
+
+document.addEventListener('DOMContentLoaded', function() {
     document.getElementById('add-filter-btn').onclick = function() {
         addSetting('new-filter', 'filter-tbl', BLOCKED_STRINGS);
     };
@@ -107,12 +198,14 @@ document.addEventListener('DOMContentLoaded', function () {
     document.getElementById('add-swwl-btn').onclick = function() {
         addSetting('new-swwl', 'swwl-tbl', SWWL);
     };
+    document.getElementById('add-cs-btn').onclick = addCustSetting;
 
     document.getElementById('clear-filter-btn').onclick = clearFilters;
     document.getElementById('clear-allow-btn').onclick = clearAllows;
     document.getElementById('clear-deny-btn').onclick = clearDeny;
     document.getElementById('clear-scrub-btn').onclick = clearScrub;
     document.getElementById('clear-swwl-btn').onclick = clearSWWL;
+    document.getElementById('clear-cs-btn').onclick = clearCustSettings;
     //var filters = ['john', 'smith', 'john smith'];
     //chrome.storage.sync.set({BLOCKED_STRINGS:filters});
     refreshSetting('filter-tbl', BLOCKED_STRINGS);
@@ -120,4 +213,7 @@ document.addEventListener('DOMContentLoaded', function () {
     refreshSetting('deny-tbl', DENY);
     refreshSetting('scrub-tbl', SCRUB);
     refreshSetting('swwl-tbl', SWWL);
+    refreshCustSettings();
+
+    addActions();
 });
--- app/scripts/storage.js
+++ app/scripts/storage.js
@@ -23,7 +23,7 @@ const ACTION_UNKNOWN = 'unknown';
  * Load user data from storage.
  */
 function getUserData() {
-    chrome.storage.sync.get(null, function (list) {
+    chrome.storage.sync.get(null, function(list) {
         stopleak.PIIData = list.hasOwnProperty(BLOCKED_STRINGS) ?  list[BLOCKED_STRINGS] : [];
         stopleak.deny = list.hasOwnProperty(DENY) ?  list[DENY] : [];
         stopleak.allow = list.hasOwnProperty(ALLOW) ?  list[ALLOW] : [];
@@ -93,7 +93,7 @@ function updateUserData(changes, areaName) {
  * @param dst destination of request
  */
 function getReqAction(src, dst) {
-    if(src === dst) {
+    if (src === dst) {
         return ACTION_ALLOW;
     }
 
@@ -101,10 +101,10 @@ function getReqAction(src, dst) {
 
     //check tuple thing -> [{src:'source domain', dst: 'dst domain', action: ACTION_*}, ...]
     len = stopleak.custSettings.length;
-    for(i = 0; i < len; i++) {
+    for (i = 0; i < len; i++) {
         var map = stopleak.custSettings[i];
-        if(map.src === src && map.dst === dst) {
-            if(map.action !== ACTION_ALLOW || map.action !== ACTION_DENY ||
+        if (map.src === src && map.dst === dst) {
+            if (map.action !== ACTION_ALLOW || map.action !== ACTION_DENY ||
                 map.action !== ACTION_SCRUB) {
 
                 console.log('storage: invalid custom setting: ' + map);
@@ -117,39 +117,38 @@ function getReqAction(src, dst) {
 
     //check deny list
     len = stopleak.deny.length;
-    for(i = 0; i < len; i++) {
-        if(stopleak.deny[i] === dst) {
+    for (i = 0; i < len; i++) {
+        if (stopleak.deny[i] === dst) {
             return ACTION_DENY;
         }
     }
 
     //check SWWL
     len = stopleak.swwl.length;
-    for(i = 0; i < len; i++) {
-        if(stopleak.swwl[i] === src) {
+    for (i = 0; i < len; i++) {
+        if (stopleak.swwl[i] === src) {
             return ACTION_ALLOW;
         }
     }
 
     //check scrub list
     len = stopleak.scrub.length;
-    for(i = 0; i < len; i++) {
-        if(stopleak.scrub[i] === dst) {
+    for (i = 0; i < len; i++) {
+        if (stopleak.scrub[i] === dst) {
             return ACTION_SCRUB;
         }
     }
 
     //check allow list
     len = stopleak.allow.length;
-    for(i = 0; i < len; i++) {
-        if(stopleak.allow[i] === dst) {
+    for (i = 0; i < len; i++) {
+        if (stopleak.allow[i] === dst) {
             return ACTION_ALLOW;
         }
     }
 
     return ACTION_UNKNOWN;
 }
-
 
 getUserData();
 chrome.storage.onChanged.addListener(updateUserData);
